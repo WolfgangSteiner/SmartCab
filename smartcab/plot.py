557c6643ee8a67f49dd2362e847f30544fe8fbae
Wolfgang Steiner <wolfgang.steiner@gmail.com>
2016-08-19 01:02:28 +0200

    Update report after review.

--- smartcab/plot.py
+++ smartcab/plot.py
@@ -2,7 +2,8 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("data_explore_first_defensive_2.csv")
+suffix = "full_state"
+df = pd.read_csv("csv/data_" + suffix + ".csv")
 print df
 
 df2 =  df.pivot(index='gamma', columns='alpha', values='success')
@@ -14,7 +15,7 @@ plt.title('success rate')
 ax = sns.heatmap(df2, annot=True, cbar=False)
 #plt.tight_layout()
 fig.set_size_inches(5,4.5)
-fig.savefig('fig/success_rate.pdf')
+fig.savefig('latex/fig/success_rate_' + suffix + '.pdf')
 #sns.plt.show()
 
 fig, ax = plt.subplots()
@@ -22,11 +23,11 @@ plt.title('penalty per step')
 ax = sns.heatmap(df3, annot=True, cbar=False)
 #plt.tight_layout()
 fig.set_size_inches(5,4.5)
-fig.savefig('fig/penalty.pdf')
+fig.savefig('latex/fig/penalty_' + suffix + '.pdf')
 
 fig, ax = plt.subplots()
 plt.title('efficiency')
 ax = sns.heatmap(df4, annot=True, cbar=False)
 #plt.tight_layout()
 fig.set_size_inches(5,4.5)
-fig.savefig('fig/efficiency.pdf')
+fig.savefig('latex/fig/efficiency_' + suffix + '.pdf')
